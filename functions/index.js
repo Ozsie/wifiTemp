79e1f66495f552af9a8df61d508f0f1749574280
Ozsie <oscar@djupfeldt.se>
2017-11-21 12:23:35 +0100

    Added function to add timestamp to post

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.addTimeStamp = functions.database.ref('/{sensorId}/{pushId}').onWrite(event => {
+  if (event.data.previous.exists()) {
+    return null;
+  }
+  return event.data.ref.parent.child(event.params.pushId).child('time').set(Date.now());
+});
