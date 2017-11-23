316cbc9cb81c38194f525a3ccc584fcbfd3023a3
Ozsie <oscar@djupfeldt.se>
2017-11-23 12:28:45 +0100

    Renamed function and added voltage conversion

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,18 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.addTimeStamp = functions.database.ref('/{sensorId}/{pushId}').onWrite(event => {
+exports.dataConverter = functions.database.ref('/{sensorId}/{pushId}').onWrite(event => {
   if (event.data.previous.exists()) {
     return null;
   }
-  return event.data.ref.parent.child(event.params.pushId).child('time').set(Date.now());
+
+  return event.data.ref.parent.child(event.params.pushId).child('time').set(Date.now()).then(function() {
+    return event.data.ref.child('voltage').once('value').then(function(snapshot) {
+      var voltage = snapshot.val();
+      if (voltage >= 1024) {
+        voltage = voltage / 1024;
+      }
+      return event.data.ref.child('voltage').set(voltage);
+    });
+  });
 });
