4e273473f1627df352d9954757741109a378e954
Ozsie <oscar@djupfeldt.se>
2017-11-23 12:29:23 +0100

    Graph styling, tooltips and average execution time calculation

--- public/index.js
+++ public/index.js
@@ -4,6 +4,43 @@ if (window.localStorage.getItem("currentSensorIndex")) {
 }
 var sensorList = [];
 
+var voltageChartSettings = {
+  axisX: {
+    labelInterpolationFnc: function skipLabels(value, index) {
+      return index % 3  === 0 ? value : null;
+    }
+  },
+  low: 0,
+  high: 4,
+  plugins: [
+    Chartist.plugins.tooltip()
+  ]
+};
+
+var signalChartSettings = {
+  axisX: {
+    labelInterpolationFnc: function skipLabels(value, index) {
+      return index % 3  === 0 ? value : null;
+    }
+  },
+  low: -90,
+  high: -20,
+  plugins: [
+    Chartist.plugins.tooltip()
+  ]
+};
+
+var tempChartSettings = {
+  axisX: {
+    labelInterpolationFnc: function skipLabels(value, index) {
+      return index % 3  === 0 ? value : null;
+    }
+  },
+  plugins: [
+    Chartist.plugins.tooltip()
+  ]
+};
+
 document.addEventListener('DOMContentLoaded', function() {
   try {
     let app = firebase.app();
@@ -28,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (temp.names[sensorId] && temp.names[sensorId].name) {
           name = temp.names[sensorId].name;
         }
-        var sensor = {id: sensorId, name: name, warnings: {}};
+        var sensor = {id: sensorId, name: name, warnings: {}, avgExecutionTime: 0};
 
         var option = document.createElement('option');
         option.text = sensor.name;
@@ -64,6 +101,10 @@ function buildArrays(temp, sensorId, sensor, timeLimit) {
   var voltage = [];
   var signal = [];
   var i = -1;
+  var minTemp = 100;
+  var maxTemp = -100;
+  var sumExecTime = 0;
+  var countExecTime = 0 ;
   for (id in temp[sensorId]) {
     i++;
     var date = temp[sensorId][id].time;
@@ -76,6 +117,8 @@ function buildArrays(temp, sensorId, sensor, timeLimit) {
       if (!m.isValid()) {
         continue;
       }
+      sumExecTime += temp[sensorId][id].runtime;
+      countExecTime++;
       var stringDate;
       if (m.isBetween(moment().startOf('day'), moment().startOf('day').minute(30))) {
         stringDate = m.format('lll');
@@ -84,12 +127,18 @@ function buildArrays(temp, sensorId, sensor, timeLimit) {
       }
       tempChartData.labels.push(stringDate);
       voltageChartData.labels.push(stringDate);
-      voltage.push(Math.round(temp[sensorId][id].voltage * 100) / 100);
+      voltage.push({meta: m.format('lll'), value: Math.round(temp[sensorId][id].voltage * 100) / 100 });
       if (temp[sensorId][id].signal) {
-        signal.push(temp[sensorId][id].signal);
+        signal.push({meta: m.format('lll'), value: temp[sensorId][id].signal });
         signalChartData.labels.push(stringDate);
       }
-      temperature.push(Math.round(temp[sensorId][id].temperature * 10) / 10);
+      temperature.push({meta: m.format('lll'), value: Math.round(temp[sensorId][id].temperature * 10) / 10 });
+      if (temp[sensorId][id].temperature > maxTemp) {
+        maxTemp = temp[sensorId][id].temperature;
+      }
+      if (temp[sensorId][id].temperature < minTemp) {
+        minTemp = temp[sensorId][id].temperature;
+      }
       if (i == Object.keys(temp[sensorId]).length - 1) {
         if (temp[sensorId][id].voltage < 3.0) {
           sensor.warnings.battery = true;
@@ -100,6 +149,9 @@ function buildArrays(temp, sensorId, sensor, timeLimit) {
       }
     }
   }
+  sensor.avgExecutionTime = (sumExecTime / countExecTime);
+  tempChartSettings.low = minTemp - 5;
+  tempChartSettings.high = maxTemp + 4;
   tempChartData.series.push(temperature);
   voltageChartData.series.push(voltage);
   signalChartData.series.push(signal);
@@ -112,7 +164,8 @@ function updateDom() {
   var current = series[series.length - 1];
   var labels = sensorList[currentSensorIndex].tempChartData.labels;
   var currentDate = labels[labels.length - 1];
-  document.getElementById('latest').innerHTML = ' ' + current + ' °C (' + currentDate + ')';
+  document.getElementById('latest').innerHTML = ' ' + current.value + ' °C, ' + currentDate;
+  document.getElementById('execTime').innerHTML = sensorList[currentSensorIndex].avgExecutionTime + ' ms';
   if (sensorList[currentSensorIndex].warnings.battery) {
     document.getElementById('batteryWarning').innerHTML = sensorList[currentSensorIndex].name + ' behöver laddas.';
   } else {
@@ -126,9 +179,9 @@ function updateDom() {
   document.getElementById('temp').innerHTML = 'Temperatur';
   document.getElementById('volt').innerHTML = 'Spänning';
   document.getElementById('sig').innerHTML = 'Signalstyrka';
-  new Chartist.Line('#temperature', sensorList[currentSensorIndex].tempChartData);
-  new Chartist.Line('#voltage', sensorList[currentSensorIndex].voltageChartData);
-  new Chartist.Line('#signal', sensorList[currentSensorIndex].signalChartData);
+  new Chartist.Line('#temperature', sensorList[currentSensorIndex].tempChartData, tempChartSettings);
+  new Chartist.Line('#voltage', sensorList[currentSensorIndex].voltageChartData, voltageChartSettings);
+  new Chartist.Line('#signal', sensorList[currentSensorIndex].signalChartData, signalChartSettings);
 }
 
 function changeSensor() {
