0d12f1bfa16fcc95c19ccb59337c50d68eb9c994
Ozsie <oscar@djupfeldt.se>
2017-11-28 17:48:58 +0100

    Added stuff to add web app to homescreen

---
+++ public/manifest.js
@@ -0,0 +1,15 @@
+{
+  "short_name": "Temp",
+  "name": "Temperature",
+  "icons": [
+    {
+      "src":"resources/icon.png",
+      "sizes": "192x192",
+      "type": "image/png  "
+    }
+  ],
+  "start_url": "/?utm_source=homescreen",
+  "background_color": "#000",
+  "Theme_color": "#536878",
+  "display": "standalone"
+}
